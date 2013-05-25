fa979041fcd31d2850eb49c5582d940b0107ec27
marta szotkowska <marta.szotkowska@gmail.com>
2013-05-25 21:56:27 +0200

    cities selection

--- pogo.rb
+++ pogo.rb
@@ -1,8 +1,42 @@
 # encoding: UTF-8
 require 'sinatra'
-get '/' do 
-	erb :index
+get '/' do
+  if params['city']
+    @city = params['city']
+  else
+    @city = 'Białystok'
+  end
+  erb :index
 end
 
+Choices = {
+  'Warszawa' => { 'row' => 406, 'col' => 250 },
+  'Białystok' => { 'row' => 379, 'col' => 285 },
+  'Bydgoszcz' => { 'row' => 381, 'col' => 199 },
 
-	
+}
+
+# <option>Białystok</option>
+#   <option>Bydgoszcz</option>
+#   <option>Ełk</option>
+#   <option>Gdańsk</option>
+#   <option>Gdynia</option>
+#   <option>Iława</option>
+#   <option>Katowice</option>
+#   <option>Koszalin</option>
+#   <option>Kraków</option>
+#   <option>Lublin</option>
+#   <option>Łomża</option>
+#   <option>Łódź</option>
+#   <option>Olszyn</option>
+#   <option>Opole</option>
+#   <option>Poznań</option>
+#   <option>Przemyśl</option>
+  # <option>Rzeszów</option>
+#   <option>Suwałki</option>
+#   <option>Szczecin</option>
+#   <option>Toruń</option>
+#   <option>Wałbrzych</option>
+#   <option>Warszawa</option>
+#   <option>Wrocław</option>
+#   <option>Zakopane</option>
