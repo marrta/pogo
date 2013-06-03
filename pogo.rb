821915c7b1f7340156cfb2b2586a71657a47cba9
marta szotkowska <marta.szotkowska@gmail.com>
2013-06-03 20:52:21 +0200

    code cleanup

--- pogo.rb
+++ pogo.rb
@@ -1,15 +1,17 @@
 # encoding: UTF-8
 require 'sinatra'
 get '/' do
+  @cities = Cities.keys.sort
   if params['city']
     @city = params['city']
   else
-    @city = 'Białystok'
+    @city = @cities.first
   end
+  @meteo_params = Cities[@city]
   erb :index
 end
 
-Choices = {
+Cities = {
   'Warszawa' => { 'row' => 406, 'col' => 250 },
   'Białystok' => { 'row' => 379, 'col' => 285 },
   'Bydgoszcz' => { 'row' => 381, 'col' => 199 },
@@ -38,27 +40,3 @@ Choices = {
 
 }
 
-# <option>Białystok</option>
-#   <option>Bydgoszcz</option>
-#   <option>Ełk</option>
-#   <option>Gdańsk</option>
-#   <option>Gdynia</option>
-#   <option>Iława</option>
-#   <option>Katowice</option>
-#   <option>Koszalin</option>
-#   <option>Kraków</option>
-#   <option>Lublin</option>
-#   <option>Łomża</option>
-#   <option>Łódź</option>
-#   <option>Olszyn</option>
-#   <option>Opole</option>
-#   <option>Poznań</option>
-#   <option>Przemyśl</option>
-  # <option>Rzeszów</option>
-#   <option>Suwałki</option>
-#   <option>Szczecin</option>
-#   <option>Toruń</option>
-#   <option>Wałbrzych</option>
-#   <option>Warszawa</option>
-#   <option>Wrocław</option>
-#   <option>Zakopane</option>
